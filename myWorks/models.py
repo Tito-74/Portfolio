e0e22643184d87489fa38d30ea633814caa249f1
Tito-74 <kipkirui133@gmail.com>
2023-05-31 21:26:43 +0300

    added myworks and message models

--- myWorks/models.py
+++ myWorks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class Message(models.Model):
+    fullName = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=50, null=True, blank=True)
+    message = models.TextField(max_length=300,null=False, blank=False)
+
+    def __str__(self):
+        return self.message
+    
+class Works(models.Model):
+    name = models.CharField(max_length=100,null=False, blank=False)
+    technologies = models.CharField(max_length=200,null=False, blank=False)
+    image = CloudinaryField('images', default=None)
+
+    def __str__(self):
+        return self.name
